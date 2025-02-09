1a566c2493ac0686abfea737734a58c703abbeae
wjiuxing <wangjiuxing2010@hotmail.com>
2025-02-09 19:22:27 +0800

    feature: 实现 Server-Sent Events

---
+++ WJXEventSource/WJXEventSourceViewer/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  WJXEventSource
+//
+//  Created by JiuxingWang on 2025/2/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
